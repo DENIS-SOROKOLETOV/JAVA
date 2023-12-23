e9b12e3f0b2e76a4067dedc199ceefe615efa517
Сороколетов Денис <PudgeIsBackInTown@mail.ru>
2023-12-23 16:14:41 +0300

    lab_2

---
+++ lab_2/src/Lab_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Lab_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str;
+        for (; scan.hasNext(); ) {
+            str =scan.next();
+            System.out.print(str + " ");
+        }
+    }
+}
