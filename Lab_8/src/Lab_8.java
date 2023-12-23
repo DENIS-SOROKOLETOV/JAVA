04be2b2dfcc1d339eb2b0685657c915954e3b0cd
Сороколетов Денис <PudgeIsBackInTown@mail.ru>
2023-12-23 16:30:49 +0300

    lab_8

---
+++ Lab_8/src/Lab_8.java
@@ -0,0 +1,7 @@
+public class Lab_8 {
+    public static void main(String[] args) {
+        Reflection.getClassInformation(String.class);
+        Reflection.getClassInformation(Integer.class);
+        Reflection.getClassInformation(Reflection.class);
+    }
+}
