740775563a831d9258521a86909791b1dc436030
Сороколетов Денис <PudgeIsBackInTown@mail.ru>
2023-12-23 16:36:46 +0300

    lab_10

---
+++ Lab_10/src/Lab_10.java
@@ -0,0 +1,16 @@
+public class Lab_10 {
+    public static void main(String[] args) {
+        Triple<Integer> triple_int = new Triple<>(10, 5, 8);
+
+        System.out.println("Min: " + triple_int.min());
+        System.out.println("Max: " + triple_int.max());
+        System.out.println("Mean: " + triple_int.mean());
+
+        Triple<String> nonMeanableTriple = new Triple<>("one", "two", "three");
+        try {
+            System.out.println("Mean: " + nonMeanableTriple.mean());
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
